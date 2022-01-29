281f49b45d9d3908657a10054f81b711ed158dc2
Sujith Varma Epuri <epurisujithvarma@gmail.com>
2022-01-29 18:50:22 +0530

    Observables added

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService{
-   ingredientsChanged = new EventEmitter<Ingredient[]>();
+   ingredientsChanged = new Subject<Ingredient[]>();
    private ingredients :Ingredient[] =[
         new Ingredient('Apple',5),
         new Ingredient('Orange',1)
@@ -12,10 +12,10 @@ export class ShoppingListService{
     }
     addItem(ingredient: Ingredient){
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice())
+        this.ingredientsChanged.next(this.ingredients.slice())
       }
       addItems(ingredients: Ingredient[]){
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice())
+        this.ingredientsChanged.next(this.ingredients.slice())
       }
 }
